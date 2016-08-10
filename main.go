601ce57b3deb34acc8fa83e14b633a40d2eb3a63
Alexey Grachov <grachov.alexey@gmail.com>
2016-08-10 16:05:36 +0300

    Simple netcat.

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+var (
+	listen = flag.Bool("l", false, "Listen")
+	host   = flag.String("h", "localhost", "Host")
+	port   = flag.Int("p", 0, "Port")
+)
+
+func main() {
+	flag.Parse()
+	if *listen {
+		startServer()
+		return
+	}
+	if len(flag.Args()) < 2 {
+		fmt.Println("Hostname and port required")
+		return
+	}
+	serverHost := flag.Arg(0)
+	serverPort := flag.Arg(1)
+	startClient(fmt.Sprintf("%s:%s", serverHost, serverPort))
+}
+
+func startServer() {
+	addr := fmt.Sprintf("%s:%d", *host, *port)
+	listener, err := net.Listen("tcp", addr)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Listening for connections on %s", listener.Addr().String())
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("Error accepting connection from client: %s", err)
+		} else {
+			go processClient(conn)
+		}
+	}
+}
+
+func processClient(conn net.Conn) {
+	_, err := io.Copy(os.Stdout, conn)
+	if err != nil {
+		log.Println(err)
+	}
+	conn.Close()
+}
+
+func startClient(addr string) {
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		fmt.Printf("Can't connect to server: %s\n", err)
+		return
+	}
+	_, err = io.Copy(conn, os.Stdin)
+	if err != nil {
+		fmt.Printf("Connection error: %s\n", err)
+	}
+}
